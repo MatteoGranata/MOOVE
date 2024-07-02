c73c12422760cfa46640c3edfa6af71f1d031372
matteo granata <matteogranata92@gmail.com>
2024-07-02 16:36:26 +0200

    create interface e class Mezzo

---
+++ src/Mezzo.ts
@@ -0,0 +1,29 @@
+interface IMezzo {
+  tipo: string; // "bici", "scooter", "monopattino"
+  idUnico: number;
+  stato: string; // "disponibile", "in uso"
+  assegnaUtente(utente: IUtente): void;
+}
+
+class Mezzo implements IMezzo {
+  tipo: string;
+  idUnico: number;
+  stato: string;
+
+  constructor(tipo: string, idUnico: number) {
+    this.tipo = tipo;
+    this.idUnico = idUnico;
+    this.stato = "disponibile";
+  }
+
+  assegnaUtente(utente: IUtente): void {
+    if (this.stato === "disponibile") {
+      this.stato = "in uso";
+      console.log(`Mezzo ${this.tipo} con ID ${this.idUnico} assegnato a ${utente.nome} ${utente.cognome}`);
+    } else {
+      console.log(`Mezzo ${this.tipo} con ID ${this.idUnico} non disponibile`);
+    }
+  }
+}
+
+export { IMezzo, Mezzo };
